a2999d2533d6035c265b52ecee0bc8c80f60e2d6
Raj <rajbaidyanath76@gmail.com>
2025-05-23 12:43:28 +0530

    done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Raj Baidyanath</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className='sm:block hidden' />
+            I love to code, build <br className='sm:block hidden' />
             interfaces and web applications
           </p>
         </div>
